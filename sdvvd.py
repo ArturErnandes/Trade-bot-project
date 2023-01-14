2c0684bc51a9ea6a1edea286da668efa89abe4a1
ArturErnandes <122566564+ArturErnandes@users.noreply.github.com>
2023-01-14 16:11:52 +0300

    Delete sdvvd.py

--- sdvvd.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-URL = 'https://yandex.com.am/weather/'
-r = requests.get(URL)
-soup = BeautifulSoup(r.text, 'html.parser')
-temperature = soup.find_all('div', class_="temp fact__temp fact__temp_size_s")
-clear_temperature = [c.text for c in temperature]
-print( clear_temperature)
